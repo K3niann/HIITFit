3f7b6adcdf09382e6a775f02f11c35d5aac546d2
Keniann Sierra <k3niann@gmail.com>
2025-01-26 08:30:49 -0500

    Page 80, Planning a Paged App

---
+++ HIITFit/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HIITFit
+//
+//  Created by Keniann Sierra on 1/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("Welcome")
+            Text("Exercise 1")
+            Text("Exercise 2")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView ()
+    }
+}
